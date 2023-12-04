e513c38d7f906927c0f358c3087c9b649b4513d6
gerdajae <101433267+gerdajae@users.noreply.github.com>
2023-12-04 21:59:25 +0200

    lisasin kommentaare

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 function reveal() {
+  // Valib kõik elemendid, millele on antud klass ".reveal"
   var reveals = document.querySelectorAll(".reveal");
 
   for (var i = 0; i < reveals.length; i++) {
+    // Salvestab lehe kõrguse
     var windowHeight = window.innerHeight;
+    // Salvestab iga elemendi ülemise piiri suhteliselt akna ülemisest servast
     var elementTop = reveals[i].getBoundingClientRect().top;
+    // Määrab, kui palju pikslitest elemendi ülemisest servast akna sisemise kõrgusega võrreldes peab olema nähtav, et klass "active" lisataks
     var elementVisible = 150;
 
+    // Kontrollib, kas elemendi ülemine piir on piisavalt allpool akna ülemist serva
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
     } else {
@@ -14,6 +19,7 @@ function reveal() {
   }
 }
 
+// Lisab 'reveal' funktsiooni kerimise sündmuse kuulajaks
 window.addEventListener("scroll", reveal);
 
 /*
